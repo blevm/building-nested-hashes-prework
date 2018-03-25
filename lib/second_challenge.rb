d38dc80a5c30de75ccf7a652fac032fcc423524b
Michelle Blevins <mjblevins7@gmail.com>
2018-03-25 21:34:42 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,9 +1,19 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
+      :patriarch => {
+        :name => "Lord Montague",
+        :age => "53"
+      }
+      :matriarch => {
+        :name => "Lady Montague",
+        :age => "54"
+      },
+      :hero => {
+        :name => "Romeo",
+        :age => "15",
+        :status => "alive"
+      },
       :hero_friends => []
       #your key/value pairs here!
       },
